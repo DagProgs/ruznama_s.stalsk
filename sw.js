1abfd2074273f02af8035372be344b73826ae874
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-06 18:56:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-
-const staticCacheName = 'static-cache-v14';
-const dynamicCacheName = 'dynamic-cache-v14';
+const staticCacheName = 'static-cache-v15';
+const dynamicCacheName = 'dynamic-cache-v15';
 
 const staticAssets = [
     './',
@@ -66,3 +65,17 @@ async function checkOnline(req) {
         }
     }
 }
+
+
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+} 
