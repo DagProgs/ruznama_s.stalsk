509692ad9b81cfc0e1ca008b22ddeff7d63c3c74
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 13:39:22 +0300

    ruznama

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-stalsk-v0';
-const dynamicCacheName = 'dynamic-stalsk-v0';
+const staticCacheName = 'static-stalsk-v1'
+const dynamicCacheName = 'dynamic-stalsk-v1'
 
 const staticAssets = [
     './',
