6d483dc7f4bcd7d0f856a83788d2df488934f4ff
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 14:14:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-stalsk-v11'
-const dynamicCacheName = 'dynamic-stalsk-v11'
+const staticCacheName = 'static-stalsk-v12'
+const dynamicCacheName = 'dynamic-stalsk-v12'
 
 const staticAssets = [
     './',
@@ -58,7 +58,7 @@ self.addEventListener('activate', event => {
             caches.keys().then(function (cacheList) {
                 return Promise.all(
                     cacheList.map(function (cacheName) {
-                        if (cacheName !== 'staticCacheName', 'dynamicCacheName') {
+                        if (cacheName !== 'staticCacheName' 'dynamicCacheName') {
                             console.log('Очистить',cacheName);
                             return caches.delete(cacheName);
                         }
@@ -69,32 +69,6 @@ self.addEventListener('activate', event => {
     );
 });
 
-self.addEventListener('fetch', event => {
-    console.log(`Trying to fetch ${event.request.url}`);
-    event.respondWith(checkCache(event.request));
-});
 
-async function checkCache(req) {
-    const cachedResponse = await caches.match(req);
-    return cachedResponse || checkOnline(req);
-}
-
-async function checkOnline(req) {
-    const cache = await caches.open(dynamicCacheName);
-    try {
-        const res = await fetch(req);
-        await cache.put(req, res.clone());
-        return res;
-    } catch (error) {
-        const cachedRes = await cache.match(req);
-        if (cachedRes) {
-            return cachedRes;
-        } else if (req.url.indexOf('.html') !== -1) {
-            return caches.match('./offline.html');
-        } else {
-            return caches.match('./images/no-image.jpg');
-        }
-    }
-}
 
 
